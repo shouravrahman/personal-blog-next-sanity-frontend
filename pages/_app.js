c1b06ac2850b40cd99f12bf2237cf9011c9be042
shouravrahman <shouravrahman006@gmail.com>
2022-01-22 16:43:07 +0600

    Revert "react-bootstrap sass"
    This reverts commit a9a89245c1480e1529455a938c809ef695dd2589.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'styles/index.scss'
+=======
+import '../styles/globals.css'
+>>>>>>> parent of a9a8924... react-bootstrap sass
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp

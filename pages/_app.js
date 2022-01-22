44a6f13e145bac0ee41096d25b751cb80755ad0c
shouravrahman <shouravrahman006@gmail.com>
2022-01-22 16:44:44 +0600

    conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'styles/index.scss'
-=======
-import '../styles/globals.css'
->>>>>>> parent of a9a8924... react-bootstrap sass
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp

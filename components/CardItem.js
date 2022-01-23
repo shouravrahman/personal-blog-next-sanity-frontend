a8e98115a83fb5491972c28ac9ed9a4fd43341c9
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 04:08:25 +0600

    primary look

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import { Card } from 'react-bootstrap'
 
-const CardItem = () => {
+const CardItem = ({ title, subtitle, image, date, author }) => {
 	return (
 		<Card className={`fj-card`}>
 			<div className='card-body-wrapper'>
 				<Card.Header className='d-flex flex-row'>
 					<img
-						src={'https://via.placeholder.com/150'}
+						src={author?.avatar || 'https://bit.ly/3IvZ3sO'}
 						className='rounded-circle mr-3'
 						height='50px'
 						width='50px'
@@ -15,17 +15,17 @@ const CardItem = () => {
 					/>
 					<div>
 						<Card.Title className='font-weight-bold mb-1'>
-							Placeholder Author
+							{author?.name || ''}
 						</Card.Title>
-						<Card.Text className='card-date'>Placeholder Date</Card.Text>
+						<Card.Text className='card-date'>{date}</Card.Text>
 					</div>
 				</Card.Header>
 				<div className='view overlay'>
-					<Card.Img src='https://via.placeholder.com/250' alt='Card image cap' />
+					<Card.Img src={image} alt='Card image cap' />
 				</div>
 				<Card.Body>
-					<Card.Title className='card-main-title'>Placeholder Title</Card.Title>
-					<Card.Text>Placehodler Subtitle</Card.Text>
+					<Card.Title className='card-main-title'>{title}</Card.Title>
+					<Card.Text>{subtitle}</Card.Text>
 				</Card.Body>
 			</div>
 			<a className='card-button'>Read More</a>

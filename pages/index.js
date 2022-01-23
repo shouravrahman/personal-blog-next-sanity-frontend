aaaf229529b835b7f35a5cbd4b073752783c3b94
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 04:09:41 +0600

    getstatic blog

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,45 @@
 /* eslint-disable @next/next/no-img-element */
 import AuthorIntro from 'components/AuthorIntro'
 import CardItem from 'components/CardItem'
-import CardListItem from 'components/CardListItem'
 import PageLayout from 'components/PageLayout'
+import { getAllBlogs } from 'lib/api'
 import { Col, Row } from 'react-bootstrap'
 
-export default function Home() {
+export default function Home({ blogs }) {
 	return (
 		<PageLayout>
 			<AuthorIntro />
 			<hr />
 			<Row className='mb-5'>
-				<Col md='10'>
+				{/* <Col md='10'>
 					<CardListItem />
-				</Col>
-				<Col md='4'>
-					<CardItem />
-				</Col>
+				</Col> */}
+				{blogs.map((blog) => (
+					<Col key={blog.slug} md='4'>
+						<CardItem
+							title={blog.title}
+							subtitle={blog.subtitle}
+							date={blog.date}
+							image={blog.coverImage}
+							author={blog.author}
+						/>
+					</Col>
+				))}
 			</Row>
 		</PageLayout>
 	)
 }
 
-// export default Home
+// This function is called during the build (build time)
+//provides props to your page
+//it will create static page
+
+export const getStaticProps = async () => {
+	const blogs = await getAllBlogs()
+
+	return {
+		props: {
+			blogs,
+		},
+	}
+}

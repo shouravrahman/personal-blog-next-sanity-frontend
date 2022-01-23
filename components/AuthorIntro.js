23447a3cfa3284378a818ffe84da397328f821fb
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 04:07:51 +0600

    intro change

--- components/AuthorIntro.js
+++ components/AuthorIntro.js
@@ -10,13 +10,13 @@ const AuthorIntro = () => (
 					width={64}
 					height={64}
 					className='mr-3'
-					src='https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4'
-					alt='Generic placeholder'
+					src='https://cdn.sanity.io/images/q9rlu9ju/production/8c6ecf78f9e596ce2a66078ff452a7bef46a1479-720x768.jpg'
+					alt='shourav rahman'
 				/>
 				<Media.Body>
 					<h5 className='font-weight-bold mb-0'>Hello Friends,</h5>
 					<p className='welcome-text'>
-						My name is Filip Jerga and I am an experienced software engineer and
+						My name is Shourav Rahman and I am an experienced web developer and
 						freelance developer. and this is my blog page.
 					</p>
 				</Media.Body>

a9e9a19cc5e4bb21e84a7baf01ab9290cd3be313
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 04:08:10 +0600

    fonts added

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,20 +1,33 @@
+/* eslint-disable @next/next/no-page-custom-font */
+import Head from 'next/head'
 import { Container } from 'react-bootstrap'
 import Navbar from './Navbar'
 
 export default function PageLayout({ children, className }) {
 	return (
-		<Container>
-			<Navbar />
-			<div className={`page-wrapper ${className}`}>{children}</div>
-			<footer className='page-footer'>
-				<div>
-					<a href='#'>courses</a>
-					{' | '}
-					<a href='#'>github</a>
-					{' | '}
-					<a href='#'>facebook</a>
-				</div>
-			</footer>
-		</Container>
+		<>
+			<Head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Indie+Flower&family=Nunito:wght@400;700;900&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
+
+			<Container>
+				<Navbar />
+				<div className={`page-wrapper ${className}`}>{children}</div>
+				<footer className='page-footer'>
+					<div>
+						<a href='#'>courses</a>
+						{' | '}
+						<a href='#'>github</a>
+						{' | '}
+						<a href='#'>facebook</a>
+					</div>
+				</footer>
+			</Container>
+		</>
 	)
 }

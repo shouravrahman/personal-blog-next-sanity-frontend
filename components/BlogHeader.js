31afcd1b7664701f65b70af85de8c86a545f8624
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 15:49:51 +0600

    blog heading

---
+++ components/BlogHeader.js
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+const BlogHeader = ({ title, subtitle, author, coverImage, date }) => {
+	return (
+		<div className='blog-detail-header'>
+			<p className='lead mb-0'>
+				<img
+					src={author?.avatar}
+					className='rounded-circle mr-3'
+					height='50px'
+					width='50px'
+					alt='avatar'
+				/>
+				{author?.name}
+				{', '} {date}
+			</p>
+			<h1 className='font-weight-bold blog-detail-header-title mb-0'>{title}</h1>
+			<h2 className='blog-detail-header-subtitle mb-3'>{subtitle}</h2>
+			{/* Check if contains cover image */}
+			<img className='img-fluid rounded' src={coverImage} alt={title} />
+		</div>
+	)
+}
+
+export default BlogHeader
